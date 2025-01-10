fe619ad482efc73df19957dd37f0570c08bd20f2
sam <samuelelolliprivato@gmail.com>
2025-01-10 05:18:59 +0100

    some fixes for deployment

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -4,7 +4,7 @@ export default function About() {
             <h2 className="text-3xl sm:text-4xl font-bold mb-4 text-[#52b788] drop-shadow-md">About Me</h2>
             <div className="px-4">
                 <p className="text-lg sm:text-xl mb-4">
-                    Hi, I'm Samuele, a dedicated computer science student with a profound passion for artificial intelligence and software engineering. I thrive on exploring the latest advancements in AI, particularly in machine learning and large language models, and enjoy diving deep into their applications to solve real-world problems. My curiosity drives me to stay up-to-date with emerging technologies and innovative trends in the field.
+                    Hi, I&apos;m Samuele, a dedicated computer science student with a profound passion for artificial intelligence and software engineering. I thrive on exploring the latest advancements in AI, particularly in machine learning and large language models, and enjoy diving deep into their applications to solve real-world problems. My curiosity drives me to stay up-to-date with emerging technologies and innovative trends in the field.
                 </p>
                 <p className="text-lg sm:text-xl mb-4">
                     For more info, download my resume above.
@@ -12,11 +12,11 @@ export default function About() {
                 <h3 className="text-2xl sm:text-3xl font-bold mb-4 text-[#52b788] drop-shadow-md">Education</h3>
                 <ul className="list-none space-y-4 mb-4">
                     <li className="text-lg sm:text-xl">
-                        <strong>Bachelor's Degree in Computer Science for Management</strong><br />
+                        <strong>Bachelor&apos;s Degree in Computer Science for Management</strong><br />
                         University of Bologna
                     </li>
                     <li className="text-lg sm:text-xl">
-                        <strong>Master's Degree in Computer Science</strong><br />
+                        <strong>Master&apos;s Degree in Computer Science</strong><br />
                         University of Bologna
                     </li>
                 </ul>
